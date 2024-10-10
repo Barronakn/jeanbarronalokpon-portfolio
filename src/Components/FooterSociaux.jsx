2e7e3503cca59c630cdba9fa54a73a2a2929db87
Barronakn <barronakn@gmail.com>
2024-10-10 05:44:40 +0100

    remove height to FooterSociaux

--- src/Components/FooterSociaux.jsx
+++ src/Components/FooterSociaux.jsx
@@ -5,7 +5,7 @@ const FooterSociaux = ({link, icon, alt}) => {
   return (
     <div className="flex flex-row justify-center items-center gap-2">
       <Link className="hover:underline" to={link}>
-        <img loading="lazy" className="w-8 h-8" src={icon} alt={alt} />
+        <img loading="lazy" className="w-8" src={icon} alt={alt} />
       </Link>
     </div>
   );
