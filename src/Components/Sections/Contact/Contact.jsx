622364d2c1eeb062131429db04cb45379ac0c806
Barronakn <barronakn@gmail.com>
2025-04-08 23:02:37 +0100

    Update Contact Page

--- src/Components/Sections/Contact/Contact.jsx
+++ src/Components/Sections/Contact/Contact.jsx
@@ -9,14 +9,14 @@ const Contact = () => {
   }, []);
 
   return (
-    <section id="contact" className="bg-sky-blue py-10">
+    <section id="contact" className="bg-sky-blue container py-10">
       <div className="mx-10 sm:mx-auto">
         <div className="text-blue-600 bg-blue-600 bg-opacity-20 text-lg rounded-xl w-40 mx-auto my-0">
           <h4 className="text-center mb-10">Contact</h4>
         </div>
 
         <div
-          className="calendly-inline-widget min-w-80 h-[700px]"
+          className="calendly-inline-widget h-[700px]"
           data-url="https://calendly.com/jeanbarronalokpon/30min"
         ></div>
       </div>
