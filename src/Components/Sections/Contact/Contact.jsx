f5361ec69e62011e8e5cd5dfc54696c10bd2d892
Barronakn <barronakn@gmail.com>
2025-04-08 22:16:32 +0100

    Update Contact Page

--- src/Components/Sections/Contact/Contact.jsx
+++ src/Components/Sections/Contact/Contact.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Contact = () => {
+
   return (
-    <div>
+    <section id="contact" className="bg-sky-blue py-10">
+      <div className="mx-10 sm:mx-auto">
+        <div className="text-blue-600 bg-blue-600 bg-opacity-20 text-lg rounded-xl w-40 mx-auto my-0">
+          <h4 className="text-center mb-10">Contact</h4>
+        </div>
+
       <div
         className="calendly-inline-widget min-w-80 h-[700px]"
         data-url="https://calendly.com/jeanbarronalokpon/30min"
@@ -12,7 +18,8 @@ const Contact = () => {
         src="https://assets.calendly.com/assets/external/widget.js"
         async
       ></script>
-    </div>
+      </div>
+    </section>
   );
 };
 
